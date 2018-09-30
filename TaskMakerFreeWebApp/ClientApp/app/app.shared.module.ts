1ba4fdba2e6c3dd810b5ad17d51c2538f5f9c8d8
michael <michael.walter@moat.co.uk>
2018-09-30 21:15:13 +0100

    initial files removed

--- TaskMakerFreeWebApp/ClientApp/app/app.shared.module.ts
+++ TaskMakerFreeWebApp/ClientApp/app/app.shared.module.ts
@@ -7,15 +7,11 @@ import { RouterModule } from '@angular/router';
 import { AppComponent } from './components/app/app.component';
 import { NavMenuComponent } from './components/navmenu/navmenu.component';
 import { HomeComponent } from './components/home/home.component';
-import { FetchDataComponent } from './components/fetchdata/fetchdata.component';
-import { CounterComponent } from './components/counter/counter.component';
 
 @NgModule({
     declarations: [
         AppComponent,
         NavMenuComponent,
-        CounterComponent,
-        FetchDataComponent,
         HomeComponent
     ],
     imports: [
@@ -24,9 +20,7 @@ import { CounterComponent } from './components/counter/counter.component';
         FormsModule,
         RouterModule.forRoot([
             { path: '', redirectTo: 'home', pathMatch: 'full' },
-            { path: 'home', component: HomeComponent },
-            { path: 'counter', component: CounterComponent },
-            { path: 'fetch-data', component: FetchDataComponent },
+            { path: 'home', component: HomeComponent }
             { path: '**', redirectTo: 'home' }
         ])
     ]
